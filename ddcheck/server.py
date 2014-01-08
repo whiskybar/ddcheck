48997a279fd78d7a414db204eac416f2a7d3fc4a
Jiri Barton <jbar@hosting4u.cz>
2014-01-09 00:53:12 +0100

    url status codes

--- ddcheck/server.py
+++ ddcheck/server.py
@@ -5,40 +5,57 @@ import argparse
 import os.path
 import eventlet
 from eventlet.timeout import Timeout
-from eventlet.green import urllib2
 from email.mime.text import MIMEText
 from eventlet.green import subprocess
 import logging
 import logging.handlers
-
+requests = eventlet.patcher.import_patched('requests')
 
 def scheduled_urls():
-    return []
+    return [('http://173.194.70.106/', 'www.google.com')]
 
-
-def check_url(url, timeout, mailto):
-    result = None
+def check_url(url, host, timeout):
+    response = None
     try:
         with Timeout(timeout, False):
-            result = urllib2.urlopen(url).read()
-        if result is None:
+            response = requests.get(url, headers={'Host': host})
+    except requests.exceptions.ConnectionError, e:
+        logging.error('%s (%s) connection failed: %s', url, host, e)
+    except requests.exceptions.HTTPError, e:
+        logging.error('%s (%s) HTTP error: %s', url, host, e)
+    except requests.exceptions.RequestException, e:
+        logging.error('%s (%s) error: %s', url, host, e)
+    else:
+        if response is None:
             logging.warning('%s timed out after %d seconds', url, timeout)
         else:
-            logging.debug('%s hit', url)
-    except urllib2.HTTPError, e:
-        logging.error('%s returned %s', url, e.code)
-    except urllib2.URLError, e:
-        logging.error('%s failed: %s', url, e.reason)
-    except Exception, e:
-        logging.error('%s fatal: %s', url, e)
+            if response.status_code == requests.codes.ok:
+                logging.debug('%s hit', url)
+            else:
+                logging.error('%s returned %s', url, e.code)
                
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Start a ddcheck daemon')
+    parser.add_argument('urls', metavar='URL', nargs='+', help='URL to check')
     parser.add_argument('-d', '--debug', default=False, action='store_true', help='debug mode on')
+    parser.add_argument('-i', '--check-interval', default=10, type=int, help='check interval')
+    parser.add_argument('-t', '--timeout', default=5, type=int, help='URL timeout')
     return parser.parse_args()
     
 def shutdown(signal, frame):
     sys.exit(0)
+
+def configure_logging(level):
+    logging.basicConfig(format='%(asctime)s %(message)s', level=level)
+    rh = logging.handlers.TimedRotatingFileHandler(
+        filename='/tmp/ddcheck.log',
+        when='midnight',
+    )
+    rh.setFormatter(logging.Formatter("%(asctime)s - %(message)s"))
+    rootLogger = logging.getLogger('')
+    rootLogger.removeHandler(rootLogger.handlers[0])
+    rootLogger.setLevel(logging.DEBUG)
+    rootLogger.addHandler(rh)
 
 def main():
     signal.signal(signal.SIGTERM, shutdown)
@@ -49,23 +66,15 @@ def main():
         level = logging.DEBUG
     else:
         level = logging.WARNING
-    logging.basicConfig(format='%(asctime)s %(message)s', level=level)
-    rh = logging.handlers.TimedRotatingFileHandler(
-        filename='/var/log/hosting/ddcheck.log',
-        when='midnight',
-    )
-    rh.setFormatter(logging.Formatter("%(asctime)s - %(message)s"))
-    rootLogger = logging.getLogger('')
-    rootLogger.removeHandler(rootLogger.handlers[0])
-    rootLogger.setLevel(logging.DEBUG)
-    rootLogger.addHandler(rh)
+    configure_logging(level)
 
+    check_interval = options.check_interval
     while True:
         now = time.time()
-        next_minute = int(now) / 60 * 60 - now + 61
-        for url, timeout, mailto in scheduled_urls():
-            eventlet.spawn_after(next_minute, check_url, url, timeout, mailto)
-        eventlet.sleep(next_minute + 1)
+        next_check = int(now) /  check_interval * check_interval - now + check_interval + 1
+        for url, host in scheduled_urls():
+            eventlet.spawn_after(next_check, check_url, url, host, options.timeout)
+        eventlet.sleep(next_check + 1)
 
 if __name__ == '__main__':
     main()
