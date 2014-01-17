98b941f0a4acc82ab2c8639078c16b6660334941
Jakub Vysoky <jakub@borka.cz>
2014-01-17 16:57:10 +0100

    newer Checkpoint

--- test_ddcheck/test_server.py
+++ test_ddcheck/test_server.py
@@ -6,7 +6,7 @@ from ddcheck.resolver import Checkpoint
 from ddcheck.server import check_url
 
 def test_non_existing_host_get_to_failed():
-    checkpoint = Checkpoint(url='http://127.0.0.1/', host='this.really.does.not.exist', record='this.really.does.not.exist.', ip='127.0.0.1')
+    checkpoint = Checkpoint(url='http://127.0.0.1/', host='this.really.does.not.exist', record='this.really.does.not.exist.', ip='127.0.0.1', type='A')
 
     failed = eventlet.Queue()
     check_url(checkpoint, failed, [], 100)
